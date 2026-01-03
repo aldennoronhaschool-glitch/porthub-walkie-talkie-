9a3834e565a07c3d5626e51e1c68d092fe09e7fa
aldennoronhaschool-glitch <aldennoronhaschool@gmail.com>
2026-01-03 13:59:22 +0530

    chore: Add more STUN servers to bypass potential firewall

--- hooks/useWebRTC.ts
+++ hooks/useWebRTC.ts
@@ -7,7 +7,11 @@ const RTC_CONFIG = {
     iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
         { urls: 'stun:stun1.l.google.com:19302' },
+        { urls: 'stun:stun2.l.google.com:19302' },
+        { urls: 'stun:stun3.l.google.com:19302' },
+        { urls: 'stun:stun4.l.google.com:19302' },
     ],
+    iceCandidatePoolSize: 10,
 };
 
 export const useWebRTC = (userId: string | undefined, activeFriendId: string | undefined) => {
